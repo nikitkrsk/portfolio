4ea857fdc0bc9dcc4052c9b06161072c9e50a663
nikitkrsk <atroshenkonikita@gmail.com>
2019-08-25 01:09:08 +0300

    Here we go

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function App() {
+  return (
+    <div className = "mainHeader">
+        <div className ="nameHeader">Nikita Atroshenko</div>
+        <div className ="codersHeader">web developer && co-founder of <a href="https://coders.group">coders.group</a></div>
+    </div>
+  );
+}
+
+export default App;
